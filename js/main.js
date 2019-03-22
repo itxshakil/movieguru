0719c250f919257f7de43443ad2d02791b06ab5a
Shakil Alam <itxshakiil@gmail.com>
2019-03-22 10:52:57 +0530

    Load More Button working Properly by Ajax

--- js/main.js
+++ js/main.js
@@ -1,80 +1,118 @@
 console.log('Connected! .');
 
 const apikey ='eaa8df42';
-let target = document.getElementById('main');
-let form = document.getElementById('searchForm');
-console.log(form);
-let searchString = form.querySelector("#search");
-let y = form.querySelector("input[type='number']");
-let type = form.querySelector("select");
-// let y = form.getElementById('year');
-// let searchString = form.getElementById('search');
-// let type = form.getElementById('type');
-form.addEventListener('submit' , search);
+const target = document.getElementById('main');
+const form = document.getElementById('searchForm');
+const searchString = form.querySelector("#search");
+const y = form.querySelector("input[type='number']");
+const type = form.querySelector("select");
+form.addEventListener('submit' , function(e){
+    e.preventDefault();
+    ajaxSearch();
+});
 target.addEventListener("click" , mainClick);
 function mainClick(e){
     if(e.target.classList.contains('btn')){
         console.log(e.target.dataset.id);
     }
 }
-function search(e){
-    e.preventDefault();
-    ajaxSearch();
+function navClickListener(e){
+    if(e.target.classList.contains('nav-link')){
+        navSearch(e.target.dataset.pagenum);
+
+    }
+}
+function nav(total , page){
+    
+    for(let result = total , i = 1 ; result > 0 ; result = result-10 , i++){
+        
+        // var loadBtn = document.createElement("button") 
+        // loadBtn.classList.add("load-btn");
+        // loadBtn.setAttribute('data-page' , page+1);
+        // var info = document.createTextNode('Load More');
+        // loadBtn.appendChild(info)
+        // target.appendChild(loadBtn);  
+    }
 }
 
-function ajaxSearch(){
+function navSearch(page){
+    ajaxSearch(page);
+}
+
+// Main Function get search result
+function ajaxSearch(page = 1){
     var xhr = new XMLHttpRequest();
-    let url = `https://www.omdbapi.com/?apikey=${apikey}&s=${searchString.value}&y=${year.value}&type=${typeValue = type.value != 'All' ? type.value : ""}`;
-    console.log(url);
+    var url = `https://www.omdbapi.com/?apikey=${apikey}&s=${searchString.value}&y=${year.value}&type=${typeValue = type.value != 'All' ? type.value : ""}`;
+    if(page != 1){
+        url += '&page=' + page;
+        console.log(url);
+        let loadBtn = document.querySelector('.load-btn');
+        loadBtn.parentNode.removeChild(loadBtn);
+    }
     xhr.open("GET", url, true);
     xhr.onreadystatechange = function () {
       console.log('Ready state Change: ' + xhr.readyState);
       if (xhr.readyState == 2) {
-        target.innerHTML = "Loading...";
+          loading = document.createElement('div');
+          let loadingText = document.createTextNode('Loading Please Wait..');
+          loading.appendChild(loadingText);
+          main.appendChild(loading);
+        console.log('State Change ', xhr.readyState);
       }
       if (xhr.readyState == 4 && xhr.status == 200){
-          target.innerHTML =null;
+          loading.parentNode.removeChild(loading);
           console.log(xhr.responseText);
           data = JSON.parse(xhr.response);
-          console.log(data.Search);
+        //   console.log(data.Search);
           for(let i=0 ,length = data.Search.length; i<length ; i++){  
-          var card = document.createElement("div");
+          let card = document.createElement("div");
           card.classList.add("card");
-          var img = document.createElement("img");
+          let img = document.createElement("img");
           img.setAttribute('height' , '320px');
           img.setAttribute('width' , '100%');
           img.setAttribute('alt' , data.Search[i].Title);
           img.setAttribute('class' , 'poster');
           img.src = data.Search[i].Poster != "N/A" ? data.Search[i].Poster : "no-poster.jpg";
           card.appendChild(img);
-          var h2 = document.createElement("h2");
+          let h2 = document.createElement("h2");
           h2.classList.add("title");
-          var title = document.createTextNode(data.Search[i].Title);
+          let title = document.createTextNode(data.Search[i].Title);
           if(title.length > 20){
               h2.style.fontSize = ".9rem";
           }
           h2.appendChild(title);
           card.appendChild(h2);
-          var h3 = document.createElement("h3");
+          let h3 = document.createElement("h3");
           h3.classList.add("year");
-          var year = document.createTextNode('Year : '+data.Search[i].Year);
+          let year = document.createTextNode('Year : '+data.Search[i].Year);
           h3.appendChild(year);
           card.appendChild(h3);
-          var h3 = document.createElement("h3");
+          h3 = document.createElement("h3");
           h3.classList.add("type");
-          var type = document.createTextNode('Type : '+data.Search[i].Type);
+          let type = document.createTextNode('Type : '+data.Search[i].Type);
           h3.appendChild(type);
           card.appendChild(h3);
-          var btn = document.createElement("button") 
+          let btn = document.createElement("button") 
           btn.classList.add("btn");
           btn.setAttribute('data-id' , data.Search[i].imdbID);
-          var info = document.createTextNode('More Info');
+          let info = document.createTextNode('More Info');
           btn.appendChild(info);
           card.appendChild(btn);
           target.appendChild(card);
           }
-
+          if(Math.ceil(data.totalResults/10) > page ){
+            const loadBtn = document.createElement("button") 
+            loadBtn.classList.add("load-btn");
+            loadBtn.setAttribute('data-page' , ++page);
+            let info = document.createTextNode('Load More');
+            loadBtn.appendChild(info)
+            target.appendChild(loadBtn); 
+            loadBtn.addEventListener('click' ,function(e){
+                ajaxSearch(e.target.dataset.page);
+            });
+          }
       }
+      
     }
     xhr.send();
 
@@ -83,7 +121,7 @@ function ajaxSearch(){
 
 //$output=file_get_contents(`http://www.omdbapi.com/?apikey=${apikey}&s=${searchString.value}`);
 
-async function fetchData(){
-    const res = await fetch(`http://www.omdbapi.com/?apikey=${apikey}&s=${searchString.value}`);
-    const json = await res.json();
-}
+// async function fetchData(){
+//     const res = await fetch(`http://www.omdbapi.com/?apikey=${apikey}&s=${searchString.value}`);
+//     const json = await res.json();
+// }
