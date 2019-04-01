ebbb827b1f2c0e16a2a346b7c8f363765e0b22db
Shakil Alam <itxshakiil@gmail.com>
2019-04-01 20:18:35 +0530

    some minor Fixes and Error Message

--- js/main.js
+++ js/main.js
@@ -52,57 +52,66 @@ function fetchResult(page = 1) {
         if (xhr.readyState == 4 && xhr.status == 200) {
             // Remove Loading Text
             loading.parentNode.removeChild(loading);
+
             let data = JSON.parse(xhr.response);
-            for (let i = 0, length = data.Search.length; i < length; i++) {
-                let card = document.createElement("div");
-                card.classList.add("card");
-                let img = document.createElement("img");
-                img.setAttribute('height', '320px');
-                img.setAttribute('width', '100%');
-                img.setAttribute('alt', data.Search[i].Title);
-                img.setAttribute('class', 'poster');
-                img.src = data.Search[i].Poster != "N/A" ? data.Search[i].Poster : "no-poster.jpg";
-                card.appendChild(img);
-                let h2 = document.createElement("h2");
-                h2.classList.add("title");
-                let title = document.createTextNode(data.Search[i].Title);
-                // Font size on basis of title length so long title dont mess card layout
-                if (title.length > 20) {
-                    h2.style.fontSize = ".9rem";
+            if (data.Response == 'False') {
+                let h3 = document.createElement('h3');
+                let error = document.createTextNode(data.Error);
+                h3.appendChild(error);
+                main.appendChild(h3);
+            } else {
+                for (let i = 0, length = data.Search.length; i < length; i++) {
+                    let card = document.createElement("div");
+                    card.classList.add("card");
+                    let img = document.createElement("img");
+                    img.setAttribute('height', '320px');
+                    img.setAttribute('width', '100%');
+                    img.setAttribute('alt', data.Search[i].Title);
+                    img.setAttribute('class', 'poster');
+                    img.src = data.Search[i].Poster != "N/A" ? data.Search[i].Poster : "no-poster.jpg";
+                    card.appendChild(img);
+                    let h2 = document.createElement("h2");
+                    h2.classList.add("title");
+                    let title = document.createTextNode(data.Search[i].Title);
+                    // Font size on basis of title length so long title dont mess card layout
+                    if (title.length > 20) {
+                        h2.style.fontSize = ".9rem";
+                    }
+                    h2.appendChild(title);
+                    card.appendChild(h2);
+                    let h3 = document.createElement("h3");
+                    h3.classList.add("year");
+                    let year = document.createTextNode('Year : ' + data.Search[i].Year);
+                    h3.appendChild(year);
+                    card.appendChild(h3);
+                    h3 = document.createElement("h3");
+                    h3.classList.add("type");
+                    let type = document.createTextNode('Type : ' + data.Search[i].Type);
+                    h3.appendChild(type);
+                    card.appendChild(h3);
+                    let btn = document.createElement("button")
+                    btn.classList.add("btn");
+                    btn.classList.add("button");
+                    btn.setAttribute('data-id', data.Search[i].imdbID);
+                    btn.setAttribute('id', 'modal-btn');
+                    let info = document.createTextNode('More Info');
+                    btn.appendChild(info);
+                    card.appendChild(btn);
+                    main.appendChild(card);
                 }
-                h2.appendChild(title);
-                card.appendChild(h2);
-                let h3 = document.createElement("h3");
-                h3.classList.add("year");
-                let year = document.createTextNode('Year : ' + data.Search[i].Year);
-                h3.appendChild(year);
-                card.appendChild(h3);
-                h3 = document.createElement("h3");
-                h3.classList.add("type");
-                let type = document.createTextNode('Type : ' + data.Search[i].Type);
-                h3.appendChild(type);
-                card.appendChild(h3);
-                let btn = document.createElement("button")
-                btn.classList.add("btn");
-                btn.classList.add("button");
-                btn.setAttribute('data-id', data.Search[i].imdbID);
-                btn.setAttribute('id', 'modal-btn');
-                let info = document.createTextNode('More Info');
-                btn.appendChild(info);
-                card.appendChild(btn);
-                main.appendChild(card);
+                if (Math.ceil(data.totalResults / 10) > page) {
+                    const loadBtn = document.createElement("button")
+                    loadBtn.classList.add("load-btn");
+                    loadBtn.setAttribute('data-page', ++page);
+                    let info = document.createTextNode('Load More');
+                    loadBtn.appendChild(info)
+                    main.appendChild(loadBtn);
+                    loadBtn.addEventListener('click', function (e) {
+                        fetchResult(e.target.dataset.page);
+                    });
+                }
             }
-            if (Math.ceil(data.totalResults / 10) > page) {
-                const loadBtn = document.createElement("button")
-                loadBtn.classList.add("load-btn");
-                loadBtn.setAttribute('data-page', ++page);
-                let info = document.createTextNode('Load More');
-                loadBtn.appendChild(info)
-                main.appendChild(loadBtn);
-                loadBtn.addEventListener('click', function (e) {
-                    fetchResult(e.target.dataset.page);
-                });
-            }
+
         }
     }
     xhr.send();
@@ -147,6 +156,9 @@ function fetchInfo(imdbID) {
             // we will look to this later
             // Object.keys(data).forEach(e => console.log(`key=${e}  value=${data[e]}`));
             for (const [key, value] of Object.entries(data)) {
+                if (key == 'Response' || key == 'Type') {
+                    continue;
+                }
                 if (key == 'Ratings') {
                     let ratings = document.createElement('h3');
                     let textratings = document.createTextNode('Ratings :');
@@ -173,7 +185,7 @@ function fetchInfo(imdbID) {
                 if (key == 'Poster') {
                     let img = document.createElement("img");
                     img.setAttribute('height', '320px');
-                    img.setAttribute('width', '100%');
+                    img.setAttribute('width', '310px');
                     img.setAttribute('alt', 'Poster');
                     img.setAttribute('class', 'poster');
                     img.src = value != "N/A" ? value : "no-poster.jpg";
@@ -191,14 +203,14 @@ function fetchInfo(imdbID) {
             modalContent.appendChild(modalBody);
             myModal.appendChild(modalContent);
             main.appendChild(myModal);
-
             const modal = document.querySelector('#my-modal');
             const modalBtn = document.querySelector('#modal-btn');
             const closeBtn = document.querySelector('.close');
 
-
-
             modal.style.display = 'block';
+
+
+            document.getElementById('my-modal').scrollIntoView();
             closeBtn.addEventListener('click', closeModal);
 
             // Events
